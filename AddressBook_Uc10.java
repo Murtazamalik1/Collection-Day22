4bf9e6989e9f6140c42ddbceee6d856aea97e912
murtaza <murtazamalik1319@gmail.com>
2023-01-20 23:11:30 +0530

    count number by city or state

---
+++ AddressBook_Uc10.java
@@ -0,0 +1,230 @@
+package day22.collection;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddressBook_Uc10 {
+    ArrayList<Contact> list = new ArrayList<>();
+    Map<Contact, String> cityDictionary = new HashMap<>();
+    Map<Contact, String> stateDictionary = new HashMap<>();
+
+    Scanner scr = new Scanner(System.in);
+
+
+    void addContact() {
+        System.out.println("Enter the first name");
+        String firstName = scr.next().toLowerCase();
+        System.out.println("Enter the last name");
+        String lastName = scr.next().toLowerCase();
+        for (Contact contacts : list) {
+            if ((contacts.getFirstName().toLowerCase().equals(firstName)) && (contacts.getLastName().toLowerCase().equals(lastName))) {
+                System.out.println("Contact already exist!!!");
+                return;
+            }
+        }
+        Contact contact = new Contact();
+        contact.setFirstName(firstName);
+        contact.setLastName(lastName);
+        System.out.print("Enter city: ");
+        contact.setCity(scr.next());
+        System.out.print("Enter state: ");
+        contact.setState(scr.next());
+        System.out.print("Enter address:  ");
+        scr.nextLine();
+        contact.setAddress(scr.nextLine());
+        System.out.print("Enter Zipcode: ");
+        contact.setZip(scr.nextInt());
+        System.out.print("Enter phone Number: ");
+        contact.setPhoneNo(scr.nextInt());
+        System.out.print("Enter email address: ");
+        contact.setEmail(scr.next());
+        list.add(contact);
+    }
+
+
+    void editContact() {
+        if (list.isEmpty()) {
+            System.out.println("Address book is empty");
+        } else {
+            System.out.println("Enter the first name of person to edit");
+            String firstName = scr.next().toLowerCase();
+            System.out.println("Enter the last name of person to edit");
+            String lastName = scr.next().toLowerCase();
+
+            boolean found = false;
+            for (Contact contact : list) {
+                if (firstName.equals(contact.getFirstName().toLowerCase())) {
+                    if (lastName.equals(contact.getLastName())) {
+                        System.out.println("Edit the details of person");
+                        System.out.print("Enter first name: ");
+                        contact.setFirstName(scr.next());
+
+                        System.out.print("Enter last Name: ");
+                        contact.setLastName(scr.next());
+
+                        System.out.print("Enter Email: ");
+                        contact.setEmail(scr.next());
+
+                        System.out.print("Enter address: ");
+                        scr.nextLine();
+                        contact.setAddress(scr.nextLine());
+
+                        System.out.print("Enter phone number: ");
+                        contact.setPhoneNo(scr.nextInt());
+
+                        System.out.print("Enter state: ");
+                        contact.setState(scr.next());
+
+                        System.out.print("Enter city: ");
+                        contact.setCity(scr.next());
+
+                        System.out.print("Enter zip: ");
+                        contact.setZip(scr.nextInt());
+                        found = true;
+                        break;
+                    }
+
+                }
+            }
+            if (!found) {
+                System.out.println("No contact found");
+            }
+        }
+    }
+
+
+    void searchContact() {
+        if (list.isEmpty()) {
+            System.out.println("No contacts to search in the addressBook");
+            return;
+        }
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("""
+                    Enter option
+                    1) To search by City
+                    2) To search by State
+                    3) To exit
+                    """);
+            int option = scr.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("Enter the city to search contacts");
+                    String city = scr.next().toLowerCase();
+                    for (AddressBook_Uc10 addressBooks : AddressBook_Uc10Main.map.values()) {
+                        for (Contact contacts : addressBooks.list) {
+                            if (contacts.getCity().toLowerCase().contains(city)) {
+                                System.out.println(contacts);
+                            }
+                        }
+                    }
+                    break;
+                case 2:
+                    System.out.println("Enter the city to search contacts");
+                    String state = scr.next().toLowerCase();
+                    for (AddressBook_Uc10 addressBooks : AddressBook_Uc10Main.map.values()) {
+                        for (Contact contacts : addressBooks.list) {
+                            if (contacts.getState().toLowerCase().contains(state)) {
+                                System.out.println(contacts);
+                            }
+                        }
+                    }
+                    break;
+                case 3:
+                    exit = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+
+    void deleteContact() {
+        if (list.isEmpty()) {
+            System.out.println("Address book is empty");
+        } else {
+            System.out.println("Enter the first name of person to delete");
+            String firstName = scr.next().toLowerCase();
+            System.out.println("Enter the last name of person to delete");
+            String lastName = scr.next().toLowerCase();
+            boolean found = false;
+            for (Contact contact : list) {
+                if (firstName.equals(contact.getFirstName().toLowerCase())) {
+                    if (lastName.equals(contact.getLastName())) {
+                        list.remove(contact);
+                        found = true;
+                        System.out.println("Contact deleted successfully");
+                        break;
+                    }
+                }
+            }
+            if (!found) {
+                System.out.println("No contact found");
+            }
+        }
+    }
+
+
+    void displayContacts() {
+        if (list.isEmpty()) {
+            System.out.println("No contacts to display");
+            return;
+        }
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("""
+                    Enter option
+                    1) To view by City
+                    2) To view by State
+                    3) To exit
+                    """);
+            int option = scr.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("Enter the city name to view");
+                    String city = scr.next().toLowerCase();
+                    for (AddressBook_Uc10 addressBooks : AddressBook_Uc10Main.map.values()) {
+                        for (Contact contacts : addressBooks.list) {
+                            if (contacts.getCity().toLowerCase().contains(city)) {
+                                cityDictionary.put(contacts, city);
+                            }
+                        }
+                    }
+                    System.out.println("No of contacts in city " + city + " are " + cityDictionary.size());
+                    System.out.println("Contacts in city " + city + " are:");
+                    System.out.println(cityDictionary.keySet());
+                    break;
+                case 2:
+                    System.out.println("Enter the state name to view");
+                    String state = scr.next().toLowerCase();
+                    for (AddressBook_Uc10 addressBooks : AddressBook_Uc10Main.map.values()) {
+                        for (Contact contacts : addressBooks.list) {
+                            if (contacts.getState().toLowerCase().contains(state)) {
+                                stateDictionary.put(contacts, state);
+                            }
+                        }
+                    }
+                    System.out.println("No of contacts in state " + state + " are " + stateDictionary.size());
+                    System.out.println("Contacts in state " + state + " are:");
+                    System.out.println(stateDictionary.keySet());
+                    break;
+                case 3:
+                    exit = true;
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return list +
+                "}\n";
+    }
+}
---
+++ AddressBook_Uc10Main.java
@@ -0,0 +1,115 @@
+package day22.collection;
+
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class AddressBook_Uc10Main {
+    static Scanner scr = new Scanner(System.in);
+    static AddressBook_Uc10 addressBook;
+    static HashMap<String, AddressBook_Uc10> map = new HashMap<>();
+    public static ArrayList<String> addressBooks = new ArrayList<>();
+    static String currentAddressBook;
+    static String addressBookName;
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Address Book Program");
+
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("""
+                    \nEnter options:
+                    1) To add contact
+                    2) To edit Contact
+                    3) To view Contacts
+                    4) To delete contact
+                    5) To add address book or select addressBook
+                    6) To search contact
+                    7) To exit""");
+
+            int option = scr.nextInt();
+            switch (option) {
+                case 1:
+                    try {
+                        map.get(currentAddressBook).addContact();
+                    } catch (Exception e) {
+                        System.out.println("\nNo AddressBook Found\n");
+                    }
+
+                    break;
+                case 2:
+                    try {
+                        map.get(currentAddressBook).editContact();
+                    } catch (Exception e) {
+                        System.out.println("\nNo AddressBook Found\n");
+                    }
+                    break;
+                case 3:
+                    try {
+                        addressBook.displayContacts();
+                    } catch (Exception e) {
+                        System.out.println("\nNo AddressBook Found\n");
+                    }
+                    break;
+                case 4:
+                    try {
+                        map.get(currentAddressBook).deleteContact();
+                    } catch (Exception e) {
+                        System.out.println("\nNo AddressBook Found\n");
+                    }
+                    break;
+                case 5:
+                    chooseAddressBook();
+                    break;
+                case 6:
+                    try {
+                        addressBook.searchContact();
+                    } catch (Exception e) {
+                        System.out.println("\nNo AddressBook Found\n");
+                    }
+                    break;
+                case 7:
+                    exit = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println(addressBooks);
+    }
+
+    static void chooseAddressBook() {
+        System.out.println("""
+                Press 1 to add AddressBook
+                Press 2 to select AddressBook""");
+
+        int option = scr.nextInt();
+        switch (option) {
+            case 1:
+                System.out.println("Enter address book name");
+                addressBookName = scr.next().toLowerCase();
+                if (map.containsKey(addressBookName)) {
+                    System.out.println("\nAddress book already exist\n");
+                    chooseAddressBook();
+                } else {
+                    addressBook = new AddressBook_Uc10();
+                    map.put(addressBookName, addressBook);
+                    currentAddressBook = addressBookName;
+                }
+                addressBooks.add(addressBookName);
+                break;
+            case 2:
+                System.out.println("Enter address book name");
+                addressBookName = scr.next().toLowerCase();
+                if (!map.containsKey(addressBookName)) {
+                    System.out.println("\nAddressBook not Found\n");
+                    chooseAddressBook();
+                } else
+                    currentAddressBook = addressBookName;
+                break;
+            default:
+                break;
+        }
+    }
+}
