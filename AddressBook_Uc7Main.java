2ddc3d9ab9baf58d9e2db8a43c418057e4cac804
murtaza <murtazamalik1319@gmail.com>
2023-01-20 22:57:18 +0530

    check for duplicate contact

---
+++ AddressBook_Uc7Main.java
@@ -0,0 +1,106 @@
+package day22.collection;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class AddressBook_Uc7Main {
+
+    static Scanner scr = new Scanner(System.in);
+    static HashMap<String, AddressBook_Uc7> map = new HashMap<>();
+    static String currentAddressBook;
+    static String addressBookName;
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to AddressBook System");
+
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("""
+                    \nEnter options:
+                    1) To add contact
+                    2) To edit Contact
+                    3) To display Contacts
+                    4) To delete contact
+                    5) To add address book or select addressBook
+                    6) To exit""");
+
+            int option = scr.nextInt();
+            switch (option) {
+                case 1:
+                    try {
+                        map.get(currentAddressBook).addContact();
+                    } catch (Exception e) {
+                        System.out.println("\nNo AddressBook Found\n");
+                    }
+
+                    break;
+                case 2:
+                    try {
+                        map.get(currentAddressBook).editContact();
+                    } catch (Exception e) {
+                        System.out.println("\nNo AddressBook Found\n");
+                    }
+                    break;
+                case 3:
+                    try {
+                        map.get(currentAddressBook).displayContacts();
+                    } catch (Exception e) {
+                        System.out.println("\nNo AddressBook Found\n");
+                    }
+                    break;
+                case 4:
+                    try {
+                        map.get(currentAddressBook).deleteContact();
+                    } catch (Exception e) {
+                        System.out.println("\nNo AddressBook Found\n");
+                    }
+                    break;
+                case 5:
+                    chooseAddressBook();
+                    break;
+                case 6:
+                    exit = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println(map);
+    }
+
+
+    static void chooseAddressBook() {
+        System.out.println("""
+                Press 1 to add AddressBook
+                Press 2 to select AddressBook""");
+
+        int option = scr.nextInt();
+        switch (option) {
+            case 1:
+                System.out.println("Enter address book name");
+                addressBookName = scr.next().toLowerCase();
+                if (map.containsKey(addressBookName)) {
+                    System.out.println("\nAddress book already exist\n");
+                    chooseAddressBook();
+                } else {
+                    AddressBook addressBook = new AddressBook();
+                    map.put(addressBookName, addressBook);
+                    currentAddressBook = addressBookName;
+                }
+
+                break;
+            case 2:
+                System.out.println("Enter address book name");
+                addressBookName = scr.next().toLowerCase();
+                if (!map.containsKey(addressBookName)) {
+                    System.out.println("\nAddressBook not Found\n");
+                    chooseAddressBook();
+                } else
+                    currentAddressBook = addressBookName;
+                break;
+            default:
+                break;
+        }
+    }
+}
